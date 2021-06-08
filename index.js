0cfffff4f047cb0e3f9cd01415abeaed7fabc222
vidhii19 <ruparelvd0919@gmail.com>
2021-06-08 19:46:28 +0530

    made html

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
+let name = "vidholi";
+
 app.get('/',function(req,res){
-    res.render('form');
+    res.render('form',{name});
 });
 
 app.listen(3000, function () {
