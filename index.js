d1f461619ccb59d69cb863f21a966eef4344ef36
madmax7874 <rushabhshah2001@gmail.com>
2021-06-09 18:09:27 +0530

    solving database bug

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const Register = require("./db/conn.js");
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
@@ -13,6 +15,19 @@ let name = "vidholi";
 app.get('/',function(req,res){
     res.render('form',{name});
 });
+
+app.get('/form',function(req,res){
+    res.render('form',{name});
+});
+
+app.post('/form',async (req,res)=>{
+    try {
+        console.log(req.body);
+        res.send(req.body);
+    } catch (error) {
+        console.log('err')
+    }
+})
 
 app.listen(3000, function () {
     console.log("Server has started on port 3000");
