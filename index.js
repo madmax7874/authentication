4bac9b4c190372a135eeb4e559b1a13d2eb08753
madmax7874 <rushabhshah2001@gmail.com>
2021-06-09 17:54:17 +0530

    db created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Register = require("./db/conn.js");
 
 app.use(express.static(path.join(__dirname, 'public')))
 
