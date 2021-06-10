8644379bbd91af33045d673d8c9e5cdcd09e433a
madmax7874 <rushabhshah2001@gmail.com>
2021-06-11 01:19:57 +0530

    login page password compaing done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const brcypt= require('bcrypt');
 const server = require('./config/db.js');
 const User = require('./models/user.js');
+const { log } = require('console');
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json());
@@ -18,7 +19,7 @@ async function securePassword(password){
     return passwordHash;
 }
 
-app.post('/',async (req,res)=>{
+app.post('/signup',async (req,res)=>{
     console.log(req.body);
     res.send(req.body); 
     const {firstName, lastName, userName, email, password} = req.body;
@@ -36,13 +37,27 @@ app.post('/',async (req,res)=>{
         console.log('no user');
     }  
 });
+app.get('/signup',(req,res)=>{
+    res.render('signup');
+});
 
+app.post('/login',async (req,res)=>{
+    console.log(req.body);
+    res.send(req.body); 
+    const {email, password} = req.body;
+    await User.findOne({email:email}).then(async (data) => {
+        const passwordFromDB = data.password;
+        const comparePassword =await brcypt.compare(password,passwordFromDB);
+        console.log(comparePassword);
+    });
+    
+})
 app.get('/login',(req,res)=>{
     res.render('login');
 });
 
 app.get('/',(req,res)=>{
-    res.render('signup');
+    res.render('home');
 });
 
 app.listen(3000, function () {
